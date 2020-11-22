d64a8bc147309883aa239903b0093f5efb7d3391
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-22 17:59:10 -0600

    added constructor in App js with state, keys, ect

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,29 @@ import './App.css';
 
 class App extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "Erik Tomlinson",
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'}
+      ],
+      home: {
+        title: 'My Portfolio',
+        subTitle: 'Here are my cool apps!',
+        subSubTitle: 'Check em out!'
+      },
+      about: {
+        title: 'About Erik Tomlinson'
+      },
+      contact: {
+        title: 'HMU'
+      }
+    }
+  }
+
     render(){
       return (
         <div> Hello World</div>
