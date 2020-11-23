98f7358675fd7e3fe8c44607ba22898217769034
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-22 18:21:04 -0600

    added router, imported router on App js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
@@ -29,7 +30,30 @@ class App extends React.Component {
 
     render(){
       return (
-        <div> Hello World</div>
+        <Router>
+        {/* <Container className="p-0" fluid={true}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Garrett Love</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar> */}
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          
+          {/* <Footer />
+
+        </Container> */}
+      </Router>
       )
     }
 };
